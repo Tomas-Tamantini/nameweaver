e84bd5eeea11402b78f6d35b37cdf6a8a6e2256a
Tomas Tamantini <tomasbft@gmail.com>
2026-02-19 21:10:01 -0300

    Install and configure vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig as defineVitestConfig } from 'vitest/config'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineVitestConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+  },
 })
