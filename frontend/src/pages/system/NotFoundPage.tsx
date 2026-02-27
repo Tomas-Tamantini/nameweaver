8a1e602aed9fb69e3d53182bd2145c5099a5a8dd
Tomas Tamantini <tomasbft@gmail.com>
2026-02-27 19:21:40 -0300

    Use empty component in not-found page

--- frontend/src/pages/system/NotFoundPage.tsx
+++ frontend/src/pages/system/NotFoundPage.tsx
@@ -1,29 +1,34 @@
 import { Button } from '@/components/ui/button'
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty'
+import { SearchX } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 function NotFoundPage() {
   return (
     <section aria-live="polite">
-      <Card>
-        <CardHeader>
-          <CardTitle>Page not found</CardTitle>
-          <CardDescription>
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <SearchX />
+          </EmptyMedia>
+          <EmptyTitle>Page not found</EmptyTitle>
+          <EmptyDescription>
             The page you are looking for does not exist.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
           <Button asChild>
             <Link to="/">Go to home page</Link>
           </Button>
-        </CardContent>
-      </Card>
+        </EmptyContent>
+      </Empty>
     </section>
   )
 }
