9af261be0c74821fffbb120e123ac090bc9bac77
Tomas Tamantini <tomasbft@gmail.com>
2026-02-28 20:10:02 -0300

    Define get people query params

--- backend/src/backend/api/routes/people.py
+++ backend/src/backend/api/routes/people.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-from backend.api.schemas.pagination import PaginatedResponse
-from backend.api.schemas.people import PersonResponse
+from fastapi import APIRouter, Query
+
+from backend.api.schemas.pagination import (
+    PaginatedResponse,
+)
+from backend.api.schemas.people import GetPeopleQueryParams, PersonResponse
 
 people_router = APIRouter(prefix="/people", tags=["people"])
 
 
 @people_router.get("/")
-def get_people() -> PaginatedResponse[PersonResponse]:
+def get_people(
+    query_params: Annotated[GetPeopleQueryParams, Query()],
+) -> PaginatedResponse[PersonResponse]:
     return PaginatedResponse(total=0, items=[])
--- backend/src/backend/api/schemas/people.py
+++ backend/src/backend/api/schemas/people.py
@@ -1,9 +1,22 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
+from fastapi import Query
 from pydantic import BaseModel, Field
+
+from backend.api.schemas.pagination import PaginationQueryParams
 
 
 class PersonResponse(BaseModel):
     id: int
     name: Annotated[str, Field(min_length=1, max_length=100)]
     description: Annotated[str, Field(min_length=1, max_length=280)]
+
+
+class GetPeopleQueryParams(PaginationQueryParams):
+    q: Annotated[
+        Optional[str],
+        Query(
+            description="Search query to filter people by name or description",
+            example="John",
+        ),
+    ] = None
