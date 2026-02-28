a11ed031c07d879c4fac128b1e0701d551651710
Tomas Tamantini <tomasbft@gmail.com>
2026-02-28 20:05:55 -0300

    Define pagination query parameters

--- backend/src/backend/api/schemas/pagination.py
+++ backend/src/backend/api/schemas/pagination.py
@@ -1,6 +1,6 @@
 from typing import Generic, Sequence, TypeVar
 
-from pydantic import BaseModel
+from pydantic import BaseModel, NonNegativeInt
 
 T = TypeVar("T")
 
@@ -8,3 +8,8 @@ T = TypeVar("T")
 class PaginatedResponse(BaseModel, Generic[T]):
     total: int
     items: Sequence[T]
+
+
+class PaginationQueryParams(BaseModel):
+    offset: NonNegativeInt = 0
+    limit: NonNegativeInt = 10
