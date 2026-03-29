21dbae41ec49f556da0ef4b776d1ee77688556a1
Tomas Tamantini <tomasbft@gmail.com>
2026-03-29 08:54:38 -0300

    Extract migration to docker

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -1,22 +1,10 @@
-from contextlib import asynccontextmanager
-
-from alembic import command
-from alembic.config import Config
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.api.routes import all_routers
 from backend.settings import get_settings
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    alembic_cfg = Config("alembic.ini")
-    command.upgrade(alembic_cfg, "head")
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 allow_origins = list(map(str.strip, get_settings().ALLOWED_ORIGINS.split(",")))
 
 app.add_middleware(
